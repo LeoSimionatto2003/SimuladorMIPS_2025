2ef5bb66b9ce142cb55f2d71b09ec8eca1655263
Leonardo Simionatto <119003560+LeoSimionatto2003@users.noreply.github.com>
2025-06-05 21:21:27 -0300

    Create test_add.asm

---
+++ test/test_add.asm
@@ -0,0 +1,6 @@
+# Teste básico de soma, subtração e adição imediata
+addi $t0, $zero, 10
+addi $t1, $zero, 20
+add $t2, $t0, $t1      # $t2 = 30
+sub $t3, $t1, $t0      # $t3 = 10
+addi $t4, $t3, 5       # $t4 = 15
